63ce7cf6c95b1ecf6ba582ae93d3318cdc1d5255
M7mdAshraf8 <m7mdashraf03@gmail.com>
2024-11-22 13:15:15 +0200

    update

---
+++ src/app/app.config.ts
@@ -0,0 +1,22 @@
+import { ApplicationConfig } from '@angular/core';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withViewTransitions,
+} from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import {
+  HttpClient,
+  HttpClientModule,
+  provideHttpClient,
+} from '@angular/common/http';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
+    provideClientHydration(),
+    provideHttpClient(),
+  ],
+};
